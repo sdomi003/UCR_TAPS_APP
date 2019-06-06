cbafcbbeccc95baf1fc5428b10a5c7b22f82da9f
KrishnaP7 <spaka001@ucr.edu>
2019-06-05 19:41:13 -0700

    Add files via upload

---
+++ java/com/example/project/Notification_Service.java
@@ -0,0 +1,28 @@
+package com.example.project;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import android.util.Log;
+
+public class Notification_Service extends FirebaseMessagingService {
+    private static final String TAG = "Notification_Service";
+
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+    @Override
+    public void onNewToken(String token) {
+        Log.d(TAG, "Refreshed token: " + token);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(token);
+    }
+
+    private void sendRegistrationToServer(String token) {
+        // TODO: Implement this method to send token to your app server.
+    }
+
+}
